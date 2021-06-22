1f400b49f220737fbc7c5c62304720c949d5792f
ndanu-josy <josphinendanu3@gmail.com>
2021-06-22 14:32:56 +0300

    Add functions to render templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,50 @@
-from flask import render_template,request,redirect,url_for,flas
+from flask import render_template,request,redirect,url_for,flash
+from . import main
+from .. import db
+from flask_login import login_user,logout_user,login_required,current_user
+from .forms import updateProfile
+from ..models import User
+
+@main.route('/',methods = ['GET'])
+def index():
+    quotes = getQuotes()
+    return render_template ('index.html',quotes = quotes)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user) 
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))   
